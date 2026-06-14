b9740f0a9d3a752e961dcfe8fea0645279cff579
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-14 21:09:04 +0530

    chore: initialize node_modules directory with backend dependencies

--- access-refresh-token/backend/src/app.js
+++ access-refresh-token/backend/src/app.js
@@ -8,7 +8,11 @@ import { authMiddleware } from "./middlewares/auth.middleware.js"
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
+
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cookieParser())

581e6078a34ee596ee9024a5425b74150980d3dd
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-15 16:39:01 +0530

    feat: implement protected and public route architecture using react-router-dom

--- access-refresh-token/frontend/src/routes/AppRoutes.jsx
+++ access-refresh-token/frontend/src/routes/AppRoutes.jsx
@@ -5,14 +5,20 @@ import Login from '../pages/Login'
 import Register from '../pages/Register'
 import Home from '../pages/Home'
 import MainLayout from '../layouts/MainLayout'
+import Public from './protected/Public'
+import Protected from './protected/Protected'
 
 const AppRoutes = () => {
 
     let router = createBrowserRouter([
         {
             path: "/",
-            element: <AuthLayout />,
+            element: <Public />,
             children: [
+                {
+                    path: "",
+                    element: <AuthLayout />,
+                    children: [
                 {
                     path: "",
                     element: <Login />                
@@ -23,15 +29,23 @@ const AppRoutes = () => {
                     element: <Register />                
                 }
             ]
+                }
+            ]
         },
 
         {
             path: "/home",
-            element: <MainLayout />,
+            element: <Protected />,
             children: [
                 {
                     path: "",
-                    element: <Home />
+                    element: <MainLayout />,
+                    children: [
+                        {
+                            path: "",
+                            element: <Home />
+                        }
+                    ]
                 }
             ]
         }

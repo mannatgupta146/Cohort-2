e1038da98181f18e0f7584cf7169e45530c3d1c3
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-16 13:09:06 +0530

    feat: dynamic routing

---
+++ nextjs/src/app/home/2/page.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const page = () => {
+  return (
+    <div>this is home page 2</div>
+  )
+}
+
+export default page
--- nextjs/src/app/layout.js
+++ nextjs/src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className= {"h-full antialiased"} >
+    <html lang="en" className= {"h-full antialiased"} suppressHydrationWarning >
       <body className="min-h-full flex flex-col">
         <Navbar />
         {children}

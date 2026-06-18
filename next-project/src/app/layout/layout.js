ca9eaa08ac4dfc7a05789f530a7121866ae443ac
Mannat Gupta <mannatgupta146@gmail.com>
2026-06-19 01:32:09 +0530

    fixed layout structure and updated links in login and product cart components

--- next-project/src/app/layout/layout.js
+++ next-project/src/app/layout/layout.js
@@ -3,23 +3,12 @@ import React from 'react'
 
 const Layout = ({ children }) => {
   return (
-    <html
-      lang="en"
-      className="dark h-full antialiased"
-      suppressHydrationWarning
-    >
-      <body
-        className="min-h-screen flex flex-col"
-        suppressHydrationWarning
-      >
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <main className="px-6 py-4 flex-1 flex flex-col">
-            {children}
-          </main>
-        </div>
-      </body>
-    </html>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="px-6 py-4 flex-1 flex flex-col">
+        {children}
+      </main>
+    </div>
   )
 }
 
